271725f16af36e0d3f511c10ce4ecc85b847b917
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-26 16:24:11 +0530

    commit

---
+++ student-course-admin-server/src/test/java/com/example/student_course_admin_server/StudentCourseAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_course_admin_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ student-course-api-gateway/src/test/java/com/example/student_course_api_gateway/StudentCourseApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_course_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
