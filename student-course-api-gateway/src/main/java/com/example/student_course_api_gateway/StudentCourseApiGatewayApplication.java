12ce45423470e1659f29ed0b4f0d7cf781b7cd1b
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-26 16:24:17 +0530

    commit

---
+++ student-course-api-gateway/src/main/java/com/example/student_course_api_gateway/StudentCourseApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_course_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseApiGatewayApplication.class, args);
+	}
+
+}
---
+++ student-course-service-registry/src/test/java/com/example/student_course_service_registry/StudentCourseServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_course_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
