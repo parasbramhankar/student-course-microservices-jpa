9b0850c39a928348ddfdfc66270021ffb1344e18
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-27 23:06:46 +0530

    commit

--- enrollment-service/src/main/java/com/example/enrollment_service/exception/StudentNotFoundException.java
+++ enrollment-service/src/main/java/com/example/enrollment_service/exception/StudentNotFoundException.java
@@ -1,4 +1,13 @@
 package com.example.enrollment_service.exception;
 
-public class StudentNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class StudentNotFoundException extends RuntimeException{
+
+    public StudentNotFoundException(String s){
+        super(s);
+    }
+
 }
